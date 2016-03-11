49e3d12a71898170a2f31cf399c252da27d68867
pagestarter <pagstarter@gmail.com>
2016-03-11 13:30:09 +0000

    Finish Static Pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,21 +4,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help"
   end
 
   test "should get contacts" do
     get :contacts
     assert_response :success
+    assert_select "title", "Contacts"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About"
   end
 
 end
